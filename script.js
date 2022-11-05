b530d797f002d239a508fd4cfdc7c68270fbc2d9
Eric <ericbyington33@gmail.com>
2022-11-04 22:36:07 -0400

    started declaring variables

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-var 
+// answer button variables
+var btnOne = document.getElementById("btn1")
+var btnTwo = document.getElementById("btn2")
+var btnThree = document.getElementById("btn3")
+var btnFour = document.getElementById("btn4")
+// homepage variables
+var rulesPage = document.getElementById("rulestext")
+var timerCount = document.getElementById("clock")
+var beginQuizBtn = document.getElementById("beginBtn")
+
+// var container = document.querySelector("#container")
+
+// container.style.display = 'none'
+
+function timerStart(){
+
+}
+
+function questionPrompt() {
+
+}
+
+
+function beginQuiz() {
+    beginQuizBtn.style.visibility = 'hidden';
+
+}
+beginQuizBtn.addEventListener("click", beginQuiz)
